a05290a01a903f818dfdf0c1f805e987c026d7e3
rachit05 <rachit.shrivastava70@gmail.com>
2021-10-14 22:04:35 +0530

    Initialize project using Create React App

---
+++ react.js
@@ -0,0 +1,16 @@
+const person = {
+    name:"Rachit",
+    age:23,
+    address: {
+        street:"Lane no 4",
+        area:"Prem vihar colony",
+        city:"Satna",
+        state:"Madhya Pradesh",
+        country : "India",
+        pin:485001
+    },
+    gender:"Male"
+}
+
+let {address} = person;
+console.log(address)
